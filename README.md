9f9143641950c10e7db64f38f0e0fbaa65601ffc
unknown <2022312034@dhvsu.edu.ph>
2025-04-15 12:07:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Cat Generator
+
+A fun little web app that fetchs and displays random cat image using the [The Cat Api](https://thecatapi.com/).
+
+---
+
+# Features
+    - Fetch random cat with just one click.
+    - Images are displayed in a gallery layout.
+
+# Tech Used
+    - HTML
+    - CSS
+    - JavaScript
+    - [The Cat API](https://thecatapi.com/) for random cat images
+
+# How to run the Project
+
+    1. Clone or Download the Repository.
+    2. Open 'index.html' in your browser.
+    3. Click "Get Random Cat" to see cats.
+
+# Future Improvements
+    - Add a "Save to Favorites" feature.
+    - Add a loading spinner when the iamge is loading.
+    - Ability to view cant infor from the API.
+    - Be able to click the image for better view or larger view.
---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const getCatBtn = document.getElementById("fetch-cat-btn");
+    const catGallery = document.getElementById("cat-gallery");
+    const displayer = document.getElementById("display");
+    const test = document.getElementById("test");
+
+    async function fetchCat() {
+        const response = await fetch("https://api.thecatapi.com/v1/images/search");
+        const data = await response.json();
+        const catImgUrl = data[0].url;
+        return catImgUrl;
+    }
+
+    getCatBtn.addEventListener('click', async () => {
+        const catImgUrl = await fetchCat();
+        const img = document.createElement("img");
+        img.src = catImgUrl;
+        img.alt = "Random Cat";
+        catGallery.appendChild(img);
+    });
+});
